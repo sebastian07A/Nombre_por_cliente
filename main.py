859e1ea818c0b80bf2feb5405523d8352623d486
sebastian07A <juansebastiananguloperez1@gmail.com>
2026-05-26 22:42:23 -0500

    mensaje del cambio

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Endpoint principal
 @app.get("/")
 def inicio():
-    return {"mensaje": ""}
+    return {
+        "mensaje": "Este es el proyecto de clientes a desarrollar"
+    }
+
+# Endpoint de clientes
+@app.get("/clientes")
+def obtener_clientes():
+    clientes = [
+        "Juan",
+        "María",
+        "Carlos",
+        "Ana",
+        "Pedro"
+    ]
+
+    return {
+        "clientes": clientes
+    }
