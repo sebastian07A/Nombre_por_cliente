ee6c30df1b21f81087f379a00a65ebcfbf187854
sebastian07A <juansebasiananguloperez1@gmail.com>
2026-06-19 16:30:50 -0500

    creando endpoints de usuario

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
-# Endpoint principal
+
 @app.get("/")
 def inicio():
-    return {
-        "mensaje": "Este es el proyecto de clientes a desarrollar"
-    }
+    return{"mensaje": "hola aprendices 3407180"}
 
-# Endpoint de clientes
-@app.get("/clientes")
-def obtener_clientes():
-    clientes = [
-        "Juan",
-        "María",
-        "Carlos",
-        "Ana",
-        "Pedro"
+@app.get("/saludar")
+def saludar():
+    return {"saludo": "hola soy..."}
+
+@app.get("/hora")
+def hora ():
+    return {"hora": datetime()}  
+       
+@app.get("saludar/{nombre}/{apellido}/{Edad}")
+def saludar2(nombre, apellido, Edad):
+    return {"saludo": f"Hola soy{nombre} {apellido} {Edad}"}
+
+@app.get("/amigos")
+def lista_amigos():
+    amigos = [
+        "Sebas",
+        "Cami",
+        "Matias",
+        "Tban",
+        "Samuel"
     ]
+    return {"amigos": amigos}
 
-    return {
-        "clientes": clientes
-    }
+@app.get("/amigos/{id}")
+def obtener_amigos(id: int):
+         return {"amigos": amigos[id]}
