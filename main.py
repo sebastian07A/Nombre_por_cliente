a4b00f6720d9b71380e193f195cf8ecd5a4c4cf6
sebastian07A <juansebasiananguloperez1@gmail.com>
2026-06-19 17:40:47 -0500

    lista de cliente, toca correccion

--- main.py
+++ main.py
@@ -33,4 +33,16 @@ def lista_amigos():
 
 @app.get("/amigos/{id}")
 def obtener_amigos(id: int):
-         return {"amigos": amigos[id]}
+         return {"amigos": amigo[id]}
+
+#usuarios
+lista_clientes = [
+    {"id": 1, "nombre":"Sebastian", "email": "sebastian@gmail.com", "edad": 18, "descripción":"N"},
+    {"id": 2, "nombre":"Cami", "email": "cami@gmail.com", "edad": 17, "descripción":"NA"},
+    {"id": 3, "nombre":"Esteban", "email": "Esteban@gmail.com", "edad": 20, "descripción":"NA"},
+    {"id": 4, "nombre":"Maria", "email": "Maria@gmail.com", "edad": 10, "descripción":"NA"},
+    {"id": 5, "nombre":"Enrique", "email": "Enrique@gmail.com", "edad": 38, "descripción":"NA"}
+]
+@app.get("/clientes")
+def listar_clientes():
+    return lista_clientes
